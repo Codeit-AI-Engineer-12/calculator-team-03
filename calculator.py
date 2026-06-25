009171c7793e4efe5b306aa45e000b3814df7c17
acaaii <everbeeeeen@gmail.com>
2026-06-25 10:13:05 +0900

    Feat: devide 연산 등록

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 from ops.mod import mod
 from ops.multiply import multiply
+from ops.divide import divide
 
-operations = {"%": mod, "*": multiply}
+operations = {"%": mod, "*": multiply, "/": devide}
