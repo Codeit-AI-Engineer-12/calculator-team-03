177d4338319e25aa1c10472dbe0025d9ce9890fd
item <taemin050@gmail.com>
2026-06-25 15:47:35 +0900

    feat: root 연산 등록

---
+++ ops/root.py
@@ -0,0 +1,19 @@
+from utils.parser import parse_natural_numbers
+
+
+def root(a=None, b=None):
+    """
+    두 자연수의 제곱근을 반환합니다.
+
+    Args:
+        a (int): 밑이 되는 자연수
+        b (int): 지수가 되는 자연수
+
+    Returns:
+        float: 두 자연수의 제곱근
+
+    Raises:
+        ValueError: 입력이 없거나, 자연수가 아니면 발생합니다.
+    """
+    a, b = parse_natural_numbers(a, b)
+    return a ** (1 / b)
