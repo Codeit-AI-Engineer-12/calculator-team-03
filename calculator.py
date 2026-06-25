d3fe2f7a9f5964c1fac8c4993b346cdcb127f9a5
Hong WooSeok <jhgh0033@gmail.com>
2026-06-25 10:15:26 +0900

    Feat: power 연산 등록

--- calculator.py
+++ calculator.py
@@ -2,4 +2,6 @@ from ops.mod import mod
 from ops.multiply import multiply
 from ops.subtract import subtract
 
-operations = {"%": mod, "*": multiply, "-": subtract}
+from ops.power import power
+
+operations = {"%": mod, "*": multiply, "-": subtract,"**": power}
