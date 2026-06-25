3b30a73b260d6379c2bef5d55b0c9051797a2e60
joelchoi85 <jymania@naver.com>
2026-06-25 16:10:47 +0900

    Feat: 계산기에 increase, decrease 함수 추가

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 from ops.add import add
 from ops.avg import avg
 from ops.divide import divide
+from ops.ido import decrease, increase
 from ops.mod import mod
 from ops.multiply import multiply
 from ops.power import power
@@ -14,4 +15,6 @@ operations = {
     "/": divide,
     "**": power,
     "avg": avg,
+    "++": increase,
+    "--": decrease,
 }
