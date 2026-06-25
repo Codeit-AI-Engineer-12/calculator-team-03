6b233cef7ce28023754916cca344e804ed8fc2f8
item <taemin050@gmail.com>
2026-06-25 14:48:06 +0900

    Docs: subtract 함수 docstring 추가

--- ops/subtract.py
+++ ops/subtract.py
@@ -2,5 +2,18 @@ from ops.parser import parse_natural_numbers
 
 
 def subtract(a=None, b=None):
+    """
+    두 자연수의 차를 반환합니다
+
+    Args:
+        a (int): 첫 번째 자연수
+        b (int): 두 번째 자연수
+
+    Returns:
+        int: 두 자연수의 차
+
+    Raises:
+        ValueError: 입력이 없거나, 자연수가 아니면 발생합니다.
+    """
     a, b = parse_natural_numbers(a, b)
     return a - b
